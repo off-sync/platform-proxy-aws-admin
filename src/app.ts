03f7080af549451da55eaac490c6bc180635d656
basverweij <bas@off-sync.com>
2017-09-13 20:01:46 +0200

    Added dynamic navigation menu and router.

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,15 @@
 export class App {
+  private router;
+
   title = 'Off-Sync.com Platform Proxy Admin for AWS';
   message = 'Hello World!';
+
+  configureRouter(config, router) {
+    this.router = router;
+
+    config.title = this.title;
+    config.map([
+      { route: ['', 'environments'], name: 'environments', moduleId: 'environments/index', title: 'Environments', nav: true },
+    ]);
+  }
 }
--- src/environments/index.ts
+++ src/environments/index.ts
@@ -2,7 +2,7 @@ import {EnvironmentsSdk} from 'sdk/environments-sdk';
 import {inject} from 'aurelia-framework';
 
 @inject(EnvironmentsSdk)
-export class EnvironmentsDashboard {
+export class Index {
   environments: string[];
 
   constructor(private environmentsSdk: EnvironmentsSdk) {
